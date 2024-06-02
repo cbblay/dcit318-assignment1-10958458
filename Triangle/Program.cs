762e1a3123f1abc0cfae838a65befa03211257a9
cbblay <cbblay@st.ug.edu.gh>
2024-06-02 20:00:51 +0000

    Third commit

---
+++ Triangle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleBot
+{
+    internal class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.Write("Enter the value for the first side of the triangle: ");
+            int sideOne = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the value for the second side of the triangle: ");
+            int sideTwo = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the value for the third side of the triangle: ");
+            int sideThree = int.Parse(Console.ReadLine());
+
+            if(sideOne == sideTwo && sideTwo == sideThree)
+            {
+                Console.Write("It is an equilateral triangle!");
+            }else if(sideOne == sideTwo || sideOne == sideThree || sideTwo == sideThree)
+            {
+                Console.Write("You have an isosceles triangle!");
+            }
+            else
+            {
+                Console.Write("The triangle is Scalene!");
+            }
+        }
+    }
+}
