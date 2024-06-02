cdbf4a38083b3fe0c28488a12f70cb77d70ae319
cbblay <cbblay@st.ug.edu.gh>
2024-06-02 17:16:39 +0000

    Second commit

---
+++ TicketPrice/Class2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketPrice
+{
+    public class Class2
+    {
+        static void Main()
+        {
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+           
+            if( age <= 12 || age >= 65)
+            {
+                Console.WriteLine("The price of your ticket is GHC7");
+            }
+            else
+            {
+                Console.WriteLine("The price of your ticket is GHC10");
+            }
+        }
+    }
+}
